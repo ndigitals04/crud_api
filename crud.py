f7548ae34c1023ee0f4c3aa1c814c59246f09505
ndigitals <ndigitals001@gmail.com>
2023-09-17 11:47:29 +0100

    Modified API to accept only a name attribute

--- crud.py
+++ crud.py
@@ -9,45 +9,34 @@ print(Path.cwd())
 app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite:///" + os.path.join(Path.cwd(), "database.db")
 db = SQLAlchemy(app)
 
-#@app.before_first_request
-#ef create_tables():
-#    db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 class PersonModel(db.Model):
-    id = db.Column(db.String(100), primary_key = True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.String(100), nullable=False)
-    track = db.Column(db.String(100), nullable=False)
-
+    name = db.Column(db.String(100),primary_key = True, nullable=False)
+    
     def __repr__(self):
-        return f"Person(name:{name}, views:{age}, likes:{track})"
+        return f"Person(name:{name})"
 
 person_put_args = reqparse.RequestParser()
-person_put_args.add_argument("user_id", type=str, help="ID of Person", required=True)
 person_put_args.add_argument("name", type=str, help="name of Person", required=True)
-person_put_args.add_argument("age", type=str, help="Age of Person", required=True)
-person_put_args.add_argument("track", type=str, help="Track of Person", required=True)
 
 person_update_args = reqparse.RequestParser()
 person_update_args.add_argument("name", type=str, help="name of Person")
-person_update_args.add_argument("age", type=str, help="age of Person")
-person_update_args.add_argument("track", type=str, help="track of Person")
 
 resource_fields = {
-    "id": fields.String,
-    "name": fields.String,
-    "age": fields.String,
-    "track": fields.String
+    "name": fields.String
 }
 
 class CreatePerson(Resource):
     @marshal_with(resource_fields)
     def put(self):
         args = person_put_args.parse_args()
-        result = PersonModel.query.filter_by(id=args["user_id"]).first()
+        result = PersonModel.query.filter_by(name=args["name"]).first()
         if result:
             abort(409, message="User ID already exists")
-        user = PersonModel(id=args["user_id"], name=args["name"], age=args["age"], track=args["track"])
+        user = PersonModel(name=args["name"])
         db.session.add(user)
         db.session.commit()
         return user, 201
@@ -56,7 +45,7 @@ class CreatePerson(Resource):
 class Person(Resource):
     @marshal_with(resource_fields)
     def get(self,user_id):
-        result = PersonModel.query.filter_by(id=user_id).first()
+        result = PersonModel.query.filter_by(name=user_id).first()
         if not result:
             abort(404, message= "could not find user with that ID")
         return  result
@@ -64,23 +53,19 @@ class Person(Resource):
     @marshal_with(resource_fields)
     def patch(self, user_id):
         args =person_update_args.parse_args()
-        result = PersonModel.query.filter_by(id=user_id).first()
+        result = PersonModel.query.filter_by(name=user_id).first()
         if not result:
             abort(404, message="User ID doesn't exists")
 
         if args["name"]:
             result.name=  args["name"]
-        if args["age"]:
-            result.age=  args["age"]
-        if args["track"]:
-            result.track=  args["track"]
-
+        
         db.session.commit()
         return result, 200
 
     @marshal_with(resource_fields)    
     def delete(self, user_id):
-        result = PersonModel.query.filter_by(id=user_id).first()
+        result = PersonModel.query.filter_by(name=user_id).first()
         if not result:
             abort(404, message="User ID doesn't exists")
 
--- testCrud.py
+++ testCrud.py
@@ -1,13 +1,13 @@
 import requests
 
-base = "https://crudapi-7j9x.onrender.com/"
-response = requests.put(base + "api", {"user_id":"1", "name":"ndigitals","age":"19","track":"backend"})
+base = "https://e494-105-113-33-134.ngrok-free.app/"
+response = requests.put(base + "api", {"user_id":"5", "name":"ndu","age":"19","track":"band"})
 print(response.json())
-response = requests.put(base + "api", {"user_id":"2", "name":"james","age":"17","track":"youtube"})
+response = requests.put(base + "api", { "name":"tutu"})
 print(response.json())
-response = requests.get(base + "api/1")
+response = requests.get(base + "api/tutu")
 print(response.json())
-response = requests.patch(base + "api/4", {"track":"frontend"})
+response = requests.patch(base + "api/ndu", {"name":"jane"})
 print(response.json())
 response = requests.delete(base + "api/1")
 print(response)
